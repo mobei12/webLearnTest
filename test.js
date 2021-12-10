4ea0787ec26899521fef5fe18e84d4cbd5156277
蒋军晖 <mobeigege@icloud.com>
2021-12-10 10:31:41 +0800

    添加点东西

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
-
-for (var index = 0; index < 5; index++) {
-    setTimeout(function (i) {
-        console.log(i);
-    }, 0, index);
-}
+var has = (nums1, nums2) => {
+	let result = [];
+	nums2.forEach(num => {
+		if (nums1.includes(num)) {
+			nums1.splice(nums1.indexOf(num), 1);
+			result.push(num);
+		}
+	});
+	return result;
+};
+console.log(has([9, 4, 9, 8, 4], [4, 9, 5]));
