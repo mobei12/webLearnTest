17d9b360be57f8fb35f060102ba39e281b6270d3
蒋军晖 <mobeigege@icloud.com>
2022-03-04 10:00:34 +0800

    更新

--- test.js
+++ test.js
@@ -1,31 +1,10 @@
 /**
- * @param {number[]} numbers
- * @return {number}
+ * @param {string[]} words
+ * @return {string}
  */
- var findShortestSubArray = function(nums) {
-    const mp = {};
+ var longestWord = function(words) {
 
-    for (const [i, num] of nums.entries()) {
-        if (num in mp) {
-            mp[num][0]++;
-            mp[num][2] = i;
-        } else {
-            mp[num] = [1, i, i];
-        }
-    }
-    console.log(Object.values(mp))
-    let maxNum = 0, minLen = 0;
-    for (const [count, left, right] of Object.values(mp)) {
-        if (maxNum < count) {
-            maxNum = count;
-            minLen = right - left + 1;
-        } else if (maxNum === count) {
-            if (minLen > (right - left + 1)) {
-                minLen = right - left + 1;
-            }
-        }
-    }
-    return minLen;
 };
-
-console.log(findShortestSubArray([1,2,2,3,1]))
+const words = ["w","wo","wor","worl", "world"]
+console.log(longestWord(words))
+longestWord()
