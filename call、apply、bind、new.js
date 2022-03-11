9153c7a3dc4eed3d204412d52ae9978323445e4d
蒋军晖 <mobeigege@icloud.com>
2022-03-11 10:19:07 +0800

    bind方法的梳理

--- call、apply、bind、new.js
+++ call、apply、bind、new.js
@@ -60,6 +60,37 @@ Function.prototype.MyBind = function (contents) {
 	res.prototype = new noop()
 	return res
 }
+//mdn版本:1.是否支持bind的判定
+if (!Function.prototype.bind) {
+	Function.prototype.bind = function (oThis) {
+		//2.绑定的是不是函数
+		if (typeof this !== 'function') {
+			// closest thing possible to the ECMAScript 5
+			// internal IsCallable function
+			throw new TypeError(
+				'Function.prototype.bind - what is trying to be bound is not callable'
+			)
+		}
+		//获取绑定时候的参数
+		var aArgs = Array.prototype.slice.call(arguments, 1),
+			fToBind = this,
+			fNOP = function () {},
+			fBound = function () {
+				return fToBind.apply(
+					this instanceof fNOP ? this : oThis,
+					aArgs.concat(Array.prototype.slice.call(arguments))
+				)
+			}
+
+		if (this.prototype) {
+			// Function.prototype doesn't have a prototype property
+			fNOP.prototype = this.prototype
+		}
+		fBound.prototype = new fNOP()
+		return fBound
+	}
+}
+
 function myNew() {
 	var obj = new Object()
 	//获取构造函数
