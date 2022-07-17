bea68cbf5b7275424947947dd5e9ec10b8c6f902
漠北 <j505453856@gmail.com>
2022-07-17 21:15:34 +0800

    更新代码

--- web/test.js
+++ web/test.js
@@ -1,6 +1,27 @@
-const haveCharacter = strVal => {
-	//匹配除了.的特殊字符
-	const testStr = RegExp(/[^\w^\s^\u4e00-\u9fa5^\u002e]/);
-	return testStr.test(strVal);
-};
-console.log(haveCharacter("124."))
+// 监听数组
+const arr = [1, 2, 3, 4];
+const newArr = new Proxy(arr, {
+	get(target, key, receiver) {
+		console.log(`get:key-${key}`);
+		return Reflect.get(target, key, receiver);
+	},
+	set(target, key, value, receiver) {
+		console.log(`set:key-${key}`);
+		return Reflect.set(target, key, value, receiver);
+	},
+	has(target, key) {
+		console.log(`has:key-${key}`);
+		return true
+	},
+	deleteProperty(target, key) {
+		console.log(`deleteProperty:${key}`);
+		return true
+	},
+	defineProperty(target, propKey, propDesc) {
+		console.log(`deleteProperty:${propKey}`);
+		return true
+	}
+});
+//newArr.push(6);
+newArr.splice(0,1)
+console.log(newArr)
