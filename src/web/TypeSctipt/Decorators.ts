673be5d0f7d6400a6a39e1cdfe4ee41a6eb4f489
mobei12 <505453856@qq.com>
2023-12-25 18:10:37 +0800

    ts 卷卷

---
+++ src/web/TypeSctipt/Decorators.ts
@@ -0,0 +1,35 @@
+{
+	class  Person{
+		name:string
+		constructor(name:string){
+			this.name = name
+		}
+		@bound
+		@logMethod
+		 greet(){
+			console.log('hello ' + this.name)
+		}
+	}
+	function logMethod(originalMethod:any,_context:ClassMethodDecoratorContext){
+		const methodName = String(_context.name)
+		function replacementMethod(this:any,...args:any[]) {
+			console.log(`LOG: Entering method '${methodName}'.`)
+			const result = originalMethod.call(this, ...args);
+			console.log(`LOG: Exiting method '${methodName}'.`)
+			return result;
+		}
+		return replacementMethod
+	}
+	function bound(originalMethod:any,context:ClassMethodDecoratorContext) {
+		console.log(context)
+		const methodName = context.name
+		if(context.private){
+			throw new Error(`'bound' cannot decorate private properties like ${methodName as string}.`);
+		}
+		context.addInitializer(function () {
+			this[methodName] = this[methodName].bind(this);
+		});
+	}
+	const person = new Person('amy')
+	person.greet()
+}
--- src/web/index.js
+++ src/web/index.js
@@ -1,52 +1,7 @@
-class EventEmitter {
-    evnets = {}
-    /**
-     * @param {string} eventName
-     * @param {Function} callback
-     * @return {Object}
-     */
-    subscribe(eventName, callback) {
-        if (this.evnets[eventName]) {
-            this.evnets[eventName].push(callback)
-        } else {
-            this.evnets[eventName] = [callback]
-        }
-        return {
-            unsubscribe: () => {
-                this.evnets[eventName] = this.evnets[eventName].filter(item => item != callback)
-                return undefined
-            }
-        };
-    }
-
-    /**
-     * @param {string} eventName
-     * @param {Array} args
-     * @return {Array}
-     */
-    emit(eventName, args = []) {
-        if (Array.isArray(this.evnets[eventName]) && (this.evnets[eventName]).length > 0) {
-            return this.evnets[eventName].map(fn => fn(...args))
-        }
-        return []
-    }
-}
-
 /**
- * const emitter = new EventEmitter();
- *
- * // Subscribe to the onClick event with onClickCallback
- * function onClickCallback() { return 99 }
- * const sub = emitter.subscribe('onClick', onClickCallback);
- *
- * emitter.emit('onClick'); // [99]
- * sub.unsubscribe(); // undefined
- * emitter.emit('onClick'); // []
+ * @param {number[]} nums
+ * @return {boolean}
  */
-const events = new EventEmitter();
-events.subscribe("dbClick", (arc) => {
-    console.log(arc);
-}, true);
-events.emit("dbClick", 1, 2, 3, 4);
-console.log(events.events)
-events.emit("dbClick", 1, 2, 3, 4);
+var canJump = function(nums) {
+
+};
