c3826e7524c2f7d0498a4303755d65d7d1827794
mobei12 <505453856@qq.com>
2024-01-15 11:28:13 +0800

    版本号排序

--- src/web/index.js
+++ src/web/index.js
@@ -1,7 +1,28 @@
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-//题目描述:有一组版本号如下['0.1.1', '2.3.3', '0.302.1', '4.2', '4.3.5', '4.3.4.5']。现在需要对其进行排序，排序的结果为 ['4.3.5','4.3.4.5','2.3.3','0.302.1','0.1.1']
-function sort(arr, target) {
-    
-}
+var arr2 = ['0.1.1', '2.3.3', '0.302.1', '4.2', '4.3.5', '4.3.4.5']
+//排序成如下 ['4.3.5','4.3.4.5','2.3.3','0.302.1','0.1.1']
 
-console.log(sort(arr, 5))
+
+/**
+ * 按指定顺序对给定的版本进行分类。
+ *
+ * @param {Array} arr - 版本数组
+ * @param {string} order - 1升序，0降序
+ * @return {Array} - The sorted array of versions.
+ */
+function sortVersion(arr, order = '1') {
+    function sortIt(current, next) {
+        const currentN = current.split('.').map(Number)
+        const nextN = next.split('.').map(Number)
+        for (let i = 0; i < Math.max(currentN.length, nextN.length); i++) {
+            const num1 = i < currentN.length ? currentN[i] : 0;
+            const num2 = i < nextN.length ? nextN[i] : 0;
+            if (num1 !== num2) {
+                return order === '1' ? num1 - num2 : num2 - num1
+            }
+
+        }
+        return 0
+    }
+    return arr.sort(sortIt)
+}
+console.log(sortVersion(arr2, '-1'))
