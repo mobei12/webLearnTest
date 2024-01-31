98ca141fa1ce399e00b26ff03abd71c4d0569933
漠北 <j505453856@gmail.com>
2024-01-31 21:57:44 +0800

    优化代码

---
+++ src/web/react/StateClass.jsx
@@ -0,0 +1,28 @@
+export default class StateClass extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isChange: false
+        };
+        this.changeState = this.changeState.bind(this);
+    }
+    render() {
+        const { isChange } = this.state;
+        return (
+            <div
+                className='abc'
+                onClick={this.changeState}
+                style={{
+                    backgroundColor: isChange ? 'skyBlue' : 'gray'
+                }}
+            >{`Hello:${isChange ? '在改一下' : '改一下'}颜色`}</div>
+        );
+    }
+    changeState() {
+        console.log(this);
+        const { isChange } = this.state;
+        this.setState({
+            isChange: !isChange
+        });
+    }
+}
--- src/web/react/app.jsx
+++ src/web/react/app.jsx
@@ -1,6 +1,7 @@
 const {createRoot} =ReactDOM
-import HelloRef from './refs.jsx'
-import LifeNew from './lifeNew.jsx'
+//import HelloRef from './refs.jsx'
+//import LifeNew from './lifeNew.jsx'
 //import Life from './life.jsx'
+import StateClass from './StateClass.jsx'
 const root = createRoot(document.getElementById('test'))
-root.render(<LifeNew/>)
+root.render(<StateClass/>)
