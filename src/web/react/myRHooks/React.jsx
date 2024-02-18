58899c68d67b6cdca26929e44f948e18d0421efc
mobei12 <505453856@qq.com>
2024-02-18 20:23:15 +0800

    实现useEffect和useReducer

--- src/web/react/myRHooks/React.jsx
+++ src/web/react/myRHooks/React.jsx
@@ -2,13 +2,16 @@ import { createRoot } from 'react-dom/client'
 export const root = createRoot(document.getElementById('test'))
 const states = []
 const stateSetters = []
-let index = 0
+let statesIndex = 0
+
+const effects = []//[[1,2]] 二维数组
+let effectsIndex = 0
 export function useState(initVal) {
-    states[index] = createState(initVal, index)
-    stateSetters.push(createStateSetter(index))
-    const _state = states[index]
-    const _setState = stateSetters[index]
-    index++
+    states[statesIndex] = createState(initVal, statesIndex)
+    stateSetters.push(createStateSetter(statesIndex))
+    const _state = states[statesIndex]
+    const _setState = stateSetters[statesIndex]
+    statesIndex++
     return [_state, _setState]
 }
 function createState(initVal, index) {
@@ -24,8 +27,37 @@ function createStateSetter(index) {
         render()
     }
 }
+export function useReducer(reducer, initialArg, init) {
+    const [state, setState] = useState(init ? init(initialArg) : initialArg)
+    function dispatch(action) {
+        const nState = reducer(state, action)
+        setState(nState)
+    }
+    return [state, dispatch]
+
+}
+export function useEffect(setUp, dependencies) {
+    if (typeof setUp !== "function") {
+        throw new TypeError('回调函数要为函数')
+    }
+    if (dependencies && !Array.isArray(dependencies)) {
+        throw new TypeError('dependencies 应该是数组')
+    }
+    let isChanged = true
+    if (effects[effectsIndex]) {
+        //effects = []//[[1,2]] 二维数组,对比当前依赖和存储不一致则更新
+        isChanged = dependencies.some((dep, index) => dep !== effects[effectsIndex][index])
+    }
+
+    if (isChanged||dependencies===undefined) {
+        setUp()
+    }
+    effects[effectsIndex] = dependencies
+    effectsIndex++
+}
 async function render() {
     const App = (await import('./app')).default;
-    index = 0
+    statesIndex = 0
+    effectsIndex = 0
     root.render(<App />)
 }
--- src/web/react/myRHooks/app.jsx
+++ src/web/react/myRHooks/app.jsx
@@ -1,18 +1,32 @@
-import { root,useState } from "./React"
+import { root, useState, useReducer, useEffect } from "./React"
 let t = null
+function reducer(state, action) {
+    if (action.type === 'incremented_age') {
+        return {
+            age: state.age + 1
+        };
+    }
+    throw Error('Unknown action.');
+}
 function App() {
     const [count, setCount] = useState(0)
     const [count1, setCount1] = useState(0)
+    const [state, dispatch] = useReducer(reducer, { age: 42 }, (initVal) => Object.assign(initVal, { age: 12 }));
+    useEffect(() => {
+        console.log(count1)
+    }, [])
     return (
-        <div style={{display: 'grid', gridTemplateColumns: 'repeat(3,1fr)',fontSize: '20px'}}>
-            <h2>{count}</h2>
+        <div >
+            <h2>{count}useState</h2>
             <button onClick={() => setCount(count + 1)}>count++</button>
             <button onClick={() => setCount(count - 1)}>count--</button>
-            <h2>{count1}</h2>
+            <h2>{count1}useState</h2>
             <button onClick={() => setCount1(count1 + 1)}>count1++</button>
             <button onClick={() => setCount1(count1 - 1)}>count1--</button>
+            <h2>{state.age}reducer</h2>
+            <button onClick={() => dispatch({ type: 'incremented_age' })}>incremented_age--</button>
         </div>
     )
 }
-root.render(<App/>)
+root.render(<App />)
 export default App
