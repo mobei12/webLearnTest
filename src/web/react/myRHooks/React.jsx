e9417eae801c2dcb07f1da13390ce0b9f5780cea
mobei12 <505453856@qq.com>
2024-02-19 14:34:22 +0800

    整理文件

--- src/web/react/myRHooks/React.jsx
+++ src/web/react/myRHooks/React.jsx
@@ -1,9 +1,14 @@
 import { createRoot } from 'react-dom/client'
 export const root = createRoot(document.getElementById('test'))
-const states = []
-const stateSetters = []
+const states = []//数据结构:[1,2,3]
+const stateSetters = []////数据结构:[fn1,fn2]
 let statesIndex = 0
-
+/**
+ * 创建新状态和状态设置器，并将它们分别添加到状态数组和状态设置器数组中的函数。
+ *
+ * @param {any} initVal - 状态的初始值
+ * @return {Array} 包含状态和状态设置器的数组
+ */
 export function useState(initVal) {
     states[statesIndex] = createState(initVal, statesIndex)
     stateSetters.push(createStateSetter(statesIndex))
@@ -12,10 +17,24 @@ export function useState(initVal) {
     statesIndex++
     return [_state, _setState]
 }
+/**
+ * 根据初始值和索引创建状态，使用states数组。
+ *
+ * @param {type} initVal - 状态的初始值
+ * @param {type} index - 用于访问states数组的索引
+ * @return {type} 基于索引的状态值，如果索引超出范围，则使用初始值
+ */
 function createState(initVal, index) {
     return states[index] ? states[index] : initVal
 }
+/**
+ * 创建一个给定索引的状态设置器函数。
+ *
+ * @param {number} index - 要设置状态的索引
+ * @return {function} 状态设置器函数
+ */
 function createStateSetter(index) {
+    //根据初始化类型的不同，缓存数据
     return (newVal) => {
         if (typeof newVal === 'function') {
             states[index] = newVal(states[index])
@@ -25,6 +44,14 @@ function createStateSetter(index) {
         render()
     }
 }
+/**
+ * 用于管理状态转换的Reducer钩子。
+ *
+ * @param {function} reducer - 用于状态转换的函数
+ * @param {any} initialArg - Reducer的初始状态或参数
+ * @param {function} init - 用于初始化状态的可选函数
+ * @return {Array} 包含当前状态和dispatch函数的数组
+ */
 export function useReducer(reducer, initialArg, init) {
     const [state, setState] = useState(init ? init(initialArg) : initialArg)
     function dispatch(action) {
@@ -37,6 +64,13 @@ export function useReducer(reducer, initialArg, init) {
 
 const effects = []//[[1,2]] 二维数组
 let effectsIndex = 0
+/**
+ * 当依赖项发生变化时执行回调函数。
+ *
+ * @param {function} setUp - 要执行的回调函数
+ * @param {Array} dependencies - 要检查变化的依赖项
+ * @return {void} 
+ */
 export function useEffect(setUp, dependencies) {
     if (typeof setUp !== "function") {
         throw new TypeError('回调函数要为函数')
@@ -56,10 +90,8 @@ export function useEffect(setUp, dependencies) {
     effects[effectsIndex] = dependencies
     effectsIndex++
 }
-
-
 const memoArr = []//[[cb,dep]],cb：回掉函数，dep：依赖
-let memoI = 0
+let useMemoIndex = 0
 /**
  * 根据依赖项对函数调用的结果进行记忆
  * @param {Function} calculate - 要进行记忆的函数
@@ -75,21 +107,21 @@ export function useMemo(calculate, dependencies) {
         throw new TypeError('dependencies 应该是数组')
     }
     // 记忆逻辑
-    if (memoArr[memoI]) {
-        const [_memo, _dep] = memoArr[memoI]
+    if (memoArr[useMemoIndex]) {
+        const [_memo, _dep] = memoArr[useMemoIndex]
         const isAllSame = dependencies && dependencies.length === _dep.length && dependencies.every((item, i) => item === _dep[i])
         if (isAllSame) {
-            memoI++
+            useMemoIndex++
 
             return _memo
         } else {
-            memoArr[memoI] = [calculate(), dependencies]
-            memoI++
+            memoArr[useMemoIndex] = [calculate(), dependencies]
+            useMemoIndex++
             return calculate()
         }
     } else {
-        memoArr[memoI] = [calculate(), dependencies]
-        memoI++
+        memoArr[useMemoIndex] = [calculate(), dependencies]
+        useMemoIndex++
         return calculate()
     }
 }
@@ -120,11 +152,12 @@ export function useCallback(fn, dependencies) {
         return fn
     }
 }
+
 async function render() {
     const App = (await import('./app')).default;
     statesIndex = 0
     effectsIndex = 0
-    memoI = 0
+    useMemoIndex = 0
     callbackIndex = 0
     root.render(<App />)
 }
