7c1c994a42435b69d076750c10c462e925272b61
mobei12 <505453856@qq.com>
2024-02-19 14:18:59 +0800

    useCallback

--- src/web/react/myRHooks/React.jsx
+++ src/web/react/myRHooks/React.jsx
@@ -57,37 +57,27 @@ export function useEffect(setUp, dependencies) {
     effectsIndex++
 }
 
-const memosDependencies = []
-const memosVal = []
-let memosIndex = 0
-export function useMemo(calculate, dependencies) {
-    if (typeof calculate !== "function") {
-        throw new TypeError('回调函数要为函数')
-    }
-    if (dependencies && !Array.isArray(dependencies)) {
-        throw new TypeError('dependencies 应该是数组')
-    }
-    let isChanged = true
-    if (memosDependencies[memosIndex]) {
-        isChanged = dependencies.some((dep, index) => dep !== memosDependencies[memosIndex][index])
-    }
-    if (isChanged) {
-        memosVal[memosIndex] = calculate()
-    }
-    return memosVal[memosIndex]
-}
-const memoArr = []//[[cb,dep]]
+
+const memoArr = []//[[cb,dep]],cb：回掉函数，dep：依赖
 let memoI = 0
-export function useMemo1(calculate, dependencies) {
+/**
+ * 根据依赖项对函数调用的结果进行记忆
+ * @param {Function} calculate - 要进行记忆的函数
+ * @param {Array} dependencies - 函数的依赖项
+ * @returns {*} - 函数的记忆结果
+ */
+export function useMemo(calculate, dependencies) {
+    // 验证输入类型
     if (typeof calculate !== "function") {
         throw new TypeError('回调函数要为函数')
     }
     if (dependencies && !Array.isArray(dependencies)) {
         throw new TypeError('dependencies 应该是数组')
     }
+    // 记忆逻辑
     if (memoArr[memoI]) {
         const [_memo, _dep] = memoArr[memoI]
-        const isAllSame = dependencies.every((item, i) => item === _dep[i])
+        const isAllSame = dependencies && dependencies.length === _dep.length && dependencies.every((item, i) => item === _dep[i])
         if (isAllSame) {
             memoI++
 
@@ -103,11 +93,38 @@ export function useMemo1(calculate, dependencies) {
         return calculate()
     }
 }
+const callbackArray = []//[[cb,dep]],cb：回掉函数，dep：依赖
+let callbackIndex = 0
+export function useCallback(fn, dependencies) {
+    if (typeof fn !== "function") {
+        throw new TypeError('回调函数要为函数')
+    }
+    if (dependencies && !Array.isArray(dependencies)) {
+        throw new TypeError('dependencies 应该是数组')
+    }
+    if (callbackArray[callbackIndex]) {
+        const [_memo, _dep] = callbackArray[callbackIndex]
+        const isAllSame = dependencies && dependencies.length === _dep.length &&
+            dependencies.every((item, i) => item === _dep[i]);
+        if (isAllSame) {
+            callbackIndex++
+            return _memo
+        } else {
+            callbackArray[callbackIndex] = [fn, dependencies]
+            callbackIndex++
+            return fn
+        }
+    } else {
+        callbackArray[callbackIndex] = [fn, dependencies]
+        callbackIndex++
+        return fn
+    }
+}
 async function render() {
     const App = (await import('./app')).default;
     statesIndex = 0
     effectsIndex = 0
-    memosIndex = 0
     memoI = 0
+    callbackIndex = 0
     root.render(<App />)
 }
--- src/web/react/myRHooks/app.jsx
+++ src/web/react/myRHooks/app.jsx
@@ -1,4 +1,4 @@
-import { root, useState, useReducer, useEffect,useMemo1 } from "./React"
+import { root, useState, useReducer, useEffect, useMemo, useCallback } from "./React"
 let t = null
 function reducer(state, action) {
     if (action.type === 'incremented_age') {
@@ -8,17 +8,36 @@ function reducer(state, action) {
     }
     throw Error('Unknown action.');
 }
+const Child = ({ onSubmit, age }) => {
+    const onclickHandle = (e) => {
+        e.preventDefault();
+        onSubmit()
+    }
+    return (
+        <div>
+            <h2>child 组件</h2>
+            <span>{age}</span>
+            <button onClick={(e) => onclickHandle(e)} >点一下</button>
+        </div>
+    )
+}
 function App() {
     const [count, setCount] = useState(0)
     const [count1, setCount1] = useState(0)
     const [state, dispatch] = useReducer(reducer, { age: 42 }, (initVal) => Object.assign(initVal, { age: 12 }));
     useEffect(() => {
-        console.log(count)
+        console.log(`useEffect log count:${count}`)
     }, [count1, count])
     useEffect(() => {
-        console.log(count1)
+        console.log(`useEffect log count1:${count1}`)
     }, [count1])
-    const memoVal = useMemo1(() => { return count * 10 }, [count,count1])
+    const memoVal = useMemo(() => { return count * 10 }, [count, count1])
+    function handleClick() {
+        setTimeout(() => {
+            console.log(`handleClick log count:${count}`)
+        }, 1000);
+    }
+    const h = useCallback(handleClick, [count])
     return (
         <div >
             <h2>{count}useState</h2>
@@ -30,8 +49,11 @@ function App() {
             <h2>{state.age}reducer</h2>
             <button onClick={() => dispatch({ type: 'incremented_age' })}>incremented_age--</button>
             <h2>{memoVal}memoVal = count*10</h2>
+            <hr />
+            <Child onSubmit={h} age='23' />
         </div>
     )
 }
+
 root.render(<App />)
 export default App
