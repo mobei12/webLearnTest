775628c4e21899990f104b4641206f724b907e5b
mobei12 <505453856@qq.com>
2024-02-19 22:07:13 +0800

    add game

--- src/web/react/app.jsx
+++ src/web/react/app.jsx
@@ -1,14 +1,17 @@
 import {createRoot} from 'react-dom/client'
-import ReduxState from '@/web/React/reduxState.tsx'
+import ReduxState from '@/web/React/reduxState.jsx'
 /* import HelloRef from './refs.jsx'
 import LifeNew from './lifeNew.jsx'
 import Life from './life.jsx'
 import StateClass from './StateClass.jsx'
 import Father from "./fatherChild.jsx" */
+import Game from './game.jsx'
+
 function App() {
     return (
         <div style={{display: 'grid', gridTemplateColumns: 'repeat(3,1fr)',fontSize: '20px'}}>
             <ReduxState/>
+            <Game/>
              {/*<HelloRef/>
             <LifeNew/>
             <Life/>
