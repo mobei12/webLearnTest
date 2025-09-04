436ff1e7813715936b834da567510a0c474d8036
mobei12 <505453856@qq.com>
2025-09-04 21:58:21 +0800

    feat:跳跃游戏 III,go解答

--- src/golang/index.go
+++ src/golang/index.go
@@ -52,7 +52,45 @@ func fib(n int) int {
 	return a
 }
 
-func finalString(s string) string {
+// https://leetcode.cn/problems/jump-game-iii/
+func DFSCanReach(arr []int, start int) bool {
+	seen := make(map[int]bool)
+	var dfs func(int) bool
+	dfs = func(i int) bool {
+		if i < 0 || i >= len(arr) {
+			return false
+		}
+		if arr[i] == 0 {
+			return true
+		}
+		if seen[i] {
+			return false
+		}
+		seen[i] = true
+		return dfs(i-arr[i]) || dfs(i+arr[i])
+	}
+	return dfs(start)
+}
+func BFSCanReach(arr []int, start int) bool {
+	seen := make(map[int]bool)
+	query := []int{start}
+	for 0 < len(query) {
+		current := query[0]
+		query = query[1:]
+		if arr[current] == 0 {
+			return true
+		}
+		for _, v := range [2]int{-1, 1} {
+			nx := current + arr[current]*v
+			if 0 <= nx && nx < len(arr) && !seen[nx] {
+				query = append(query, int(nx))
+				seen[nx] = true
+			}
+		}
+	}
+	return false
+}
+func finalString(s string) string { //输入string，输出rtsng
 	q := [2][]byte{} // 两个 slice 背靠背，q[0] 向左，q[1] 向右
 	dir := 1
 	for _, c := range s {
@@ -62,11 +100,11 @@ func finalString(s string) string {
 			q[dir] = append(q[dir], byte(c))
 		}
 	}
-	fmt.Println(q)
 	slices.Reverse(q[dir^1])
 	return string(append(q[dir^1], q[dir]...))
 }
 func main() {
-	str := "striacing"
-	fmt.Printf(finalString(str))
+	arr := []int{4, 2, 3, 0, 3, 1, 2}
+	start := 0
+	fmt.Println(BFSCanReach(arr, start))
 }
