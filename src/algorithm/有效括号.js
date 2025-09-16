16211b014f83ee38adda7360028ca4f40e298993
mobei12 <505453856@qq.com>
2025-09-16 18:07:27 +0800

    refresh

--- src/algorithm/有效括号.js
+++ src/algorithm/有效括号.js
@@ -3,22 +3,42 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-	const stMap = new Map([
-		['(', ')'],
-		['{', '}'],
-		['[', ']']
-	]);
-	const sArr = s.split('');
-	const stakStr = [sArr[0]];
-	for (let i = 1; i < sArr.length; i++) {
-		const element = sArr[i];
-		if (stMap.get(stakStr.slice(-1)[0]) === element) {
-			stakStr.splice(-1);
-		} else {
-			stakStr.push(element);
-		}
-	}
-	return stakStr.length === 0;
+  const stMap = new Map([
+    ["(", ")"],
+    ["{", "}"],
+    ["[", "]"],
+  ]);
+  const sArr = s.split("");
+  const stakStr = [sArr[0]];
+  for (let i = 1; i < sArr.length; i++) {
+    const element = sArr[i];
+    if (stMap.get(stakStr.slice(-1)[0]) === element) {
+      stakStr.splice(-1);
+    } else {
+      stakStr.push(element);
+    }
+  }
+  return stakStr.length === 0;
 };
-const str = '{[]}';
-console.log(isValid(str));
+const IsValidParentheses = (str) => {
+  if (str.length % 2 !== 0) return false;
+  const deqArr = [];
+  const leftP = "]})";
+  for (let i = 0; i < str.length; i++) {
+    const element = str[i];
+    if (leftP.includes(element) && deqArr === 0) {
+      return false;
+    } else if (
+      (element === "]" && deqArr[deqArr.length - 1] === "[") ||
+      (element === ")" && deqArr[deqArr.length - 1] === "(") ||
+      (element === "}" && deqArr[deqArr.length - 1] === "{")
+    ) {
+      deqArr.pop();
+    } else {
+      deqArr.push(element);
+    }
+  }
+  return deqArr.length === 0;
+};
+const str = "((}";
+console.log(IsValidParentheses(str));
